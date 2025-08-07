0c5251999430fffdf1f9d7f91118169d0cce16a6
fearonc <connor_fearon@hotmail.co.uk>
2025-08-07 16:05:53 +0100

    script.js

--- script.js
+++ script.js
@@ -19,3 +19,11 @@ function addTask() {
   document.getElementById('taskList').appendChild(li);
   input.value = '';
 }
+
+document.addEventListener('mousemove', function(e) {
+  const xOffset = (e.clientX / window.innerWidth - 0.5) * 20;
+  const yOffset = (e.clientY / window.innerHeight - 0.5) * 20;
+
+  document.body.style.backgroundPosition = `calc(50% + ${xOffset}px) calc(50% + ${yOffset}px)`;
+});
+
